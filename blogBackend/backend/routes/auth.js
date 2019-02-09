c67aabfa5536598da337adc2c463bdbf627d71e0
Zuoli Zhu <zuolizhu5960x@gmail.com>
2019-02-09 00:12:54 -0500

    authenticated user with jwt

--- blogBackend/backend/routes/auth.js
+++ blogBackend/backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -21,6 +22,42 @@ router.post("/signup", (req, res, next) => {
             res.status(500).json({
                 error: err
             });
+        });
+    });
+});
+
+router.post("/login", (req, res, next) => {
+    let fetchedUser;
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if(!user) {
+            return res.status(401).json({
+                message: 'Email does not exist!'
+            });
+        }
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+        if(!result) {
+            return res.status(401).json({
+                message: 'Password does not match!'
+            });
+        }
+
+        // JWT config
+        const token = jwt.sign(
+            {email: fetchedUser.email, userId: fetchedUser._id}, 
+            'secret_key_for_dev_only', 
+            {expiresIn: "1h"});
+
+        // Send token to front end
+        res.status(200).json({
+            token: token
+        });
+    }).catch(err => {
+        return res.status(401).json({
+            message: 'Auth failed ...'
         });
     });
 });
