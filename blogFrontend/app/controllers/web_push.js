59ca93f2f8a61842b5337290d68f142a43297e9b
Zuoli Zhu <zuolizhu5960x@gmail.com>
2019-02-11 20:52:56 -0500

    notification displayed when button click

--- blogFrontend/app/controllers/web_push.js
+++ blogFrontend/app/controllers/web_push.js
@@ -1,27 +1,15 @@
-var littleBlog = angular.module('littleBlog', ['ngSanitize', 'angular-web-notification']);
-
-littleBlog.controller('mainController', function PostListController($scope, $http){
-    
-    $scope.title = 'Example Notification';
-    $scope.text = 'This is some notification text.';
-
-    // Get all posts from database
-    $http.get('http://localhost:3000/api/posts/')
-    .then(function fetchedPosts(response){
-        $scope.myPosts = response.data.posts;
-    }, function fetchingError(response){
-        console.log("error: " + response);
-    });
-});
-
-// Notification controller
-littleBlog.controller('exampleForm', ['$scope', function ($scope) {
+window.angular.module('littleBlog', [
+    'angular-web-notification'
+]).controller('exampleForm', ['$scope', function ($scope) {
     'use strict';
+
     $scope.title = 'Little Blog Update';
     $scope.text = 'There is a new article uploaded!';
 }]).directive('showButton', ['webNotification', function (webNotification) {
     'use strict';
+
     var serviceWorkerRegistration;
+
     if (navigator.serviceWorker) {
         navigator.serviceWorker.register('service-worker.js').then(function (registration) {
             serviceWorkerRegistration = registration;
@@ -42,17 +30,17 @@ littleBlog.controller('exampleForm', ['$scope', function ($scope) {
                     onClick: function onNotificationClicked() {
                         console.log('Notification clicked.');
                     },
-                    autoClose: 6000 //auto close the notification after 6 seconds
+                    autoClose: 4000 //auto close the notification after 4 seconds (you can manually close it via hide function)
                 }, function onShow(error, hide) {
                     if (error) {
                         window.alert('Unable to show notification: ' + error.message);
                     } else {
                         console.log('Notification Shown.');
-                        // If autoClose does not work
-                        // setTimeout(function hideNotification() {
-                        //     console.log('Hiding notification....');
-                        //     hide(); //manually close the notification
-                        // }, 7000);
+
+                        setTimeout(function hideNotification() {
+                            console.log('Hiding notification....');
+                            hide();
+                        }, 5000);
                     }
                 });
             });
