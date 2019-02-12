91a26d832e14a9da16ab39d637b594de9d8eab61
Zuoli Zhu <zuolizhu5960x@gmail.com>
2019-02-12 18:40:15 -0500

    remove notification plug in, not useful

--- blogFrontend/app.js
+++ blogFrontend/app.js
@@ -1,5 +1,5 @@
 var littleBlog = angular
-    .module('littleBlog', ['ngSanitize', 'angular-web-notification', 'ui.router', 'ngStorage', 'ui.tinymce'])
+    .module('littleBlog', ['ngSanitize', 'ui.router', 'ngStorage', 'ui.tinymce'])
     .config(config)
     .run(run);
 
@@ -46,41 +46,3 @@ function run($rootScope, $http, $location, $localStorage){
         }
     });
 }
-
-// Notification controller
-littleBlog.controller('exampleForm', ['$scope', function ($scope) {
-    'use strict';
-    $scope.title = 'Little Blog Update';
-    $scope.text = 'There is a new article uploaded!';
-}]).directive('showButton', ['webNotification', function (webNotification) {
-    'use strict';
-    var serviceWorkerRegistration;
-    if (navigator.serviceWorker) {
-        navigator.serviceWorker.register('service-worker.js').then(function (registration) {
-            serviceWorkerRegistration = registration;
-        });
-    }
-
-    return {
-        restrict: 'C',
-        scope: {
-            notificationTitle: '=',
-            notificationText: '='
-        },
-        link: function (scope, element) {
-            element.on('click', function onClick() {
-                webNotification.showNotification(scope.notificationTitle, {
-                    serviceWorkerRegistration: serviceWorkerRegistration,
-                    body: scope.notificationText,
-                    autoClose: 6000 //auto close the notification after 6 seconds
-                }, function onShow(error, hide) {
-                    if (error) {
-                        window.alert('Unable to show notification: ' + error.message);
-                    } else {
-                        console.log('Notification Shown.');
-                    }
-                });
-            });
-        }
-    };
-}]);
